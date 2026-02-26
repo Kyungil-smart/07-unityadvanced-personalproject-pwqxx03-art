5ecd495cdf21519775867f75223888f1e50b135d
pwqxx03-art <pwqxx03@gmail.com>
2026-02-27 03:35:19 +0900

    플레어 점프 및 이미지 구축

--- plump/Assets/Scripts/Player/PlayerBall.cs
+++ plump/Assets/Scripts/Player/PlayerBall.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 
 public class PlayerBall : MonoBehaviour
 {
+    public float jumpForce;
     Rigidbody rigid;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody>();
     }
-    
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigid.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+        }
+    }
     void FixedUpdate()
     {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
-        
+
         rigid.AddForce(new Vector3(h, 0, v), ForceMode.Impulse);
     }
 }
